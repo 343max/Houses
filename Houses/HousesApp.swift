1c7ea029781ee16b74678af6ca05ba9c23ef729c
Max von Webel <max@343max.de>
2022-09-02 08:20:15 +0200

    Initial Commit

---
+++ Houses/HousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousesApp.swift
+//  Houses
+//
+//  Created by Max von Webel on 02.09.22.
+//
+
+import SwiftUI
+
+@main
+struct HousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
